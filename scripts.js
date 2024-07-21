54a972bbfb16c514b6d4072eca4ed57e074a5387
Connect2Sakthi <connect2sakthi85@gmail.com>
2024-07-21 16:14:04 +0800

    testing page

--- scripts.js
+++ scripts.js
@@ -2,3 +2,19 @@
 //     // Add any JavaScript functionality if needed
 //     console.log('Document loaded');
 // });
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const slides = document.querySelectorAll('.slider .slide');
+//     let currentSlide = 0;
+
+//     function nextSlide() {
+//         slides[currentSlide].classList.remove('active');
+//         slides[currentSlide].classList.add('previous');
+//         currentSlide = (currentSlide + 1) % slides.length;
+//         slides[currentSlide].classList.add('active');
+//         slides[(currentSlide - 1 + slides.length) % slides.length].classList.remove('previous');
+//     }
+
+//     setInterval(nextSlide, 3000); // Change slide every 3 seconds
+// });
